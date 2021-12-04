10943102b9e7ccf5d0157a569f602f254acc1a2f
Marty <Zexyqag@gmail.com>
2021-12-04 17:22:53 +0100

    Update Platform.prefab.meta, Win.prefab, and Win.cs

--- Assets/Win.cs
+++ Assets/Win.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Win : MonoBehaviour
-{
+public class Win : MonoBehaviour {
+	public UnityEvent onTriggerEnter;
+
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		Debug.Log("u win");
+		onTriggerEnter.Invoke();
 	}
 }
