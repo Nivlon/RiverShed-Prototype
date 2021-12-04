1245c91799bd18e65d25adc14c51c195e4c60b01
Fabian <fabiandemeter22@gmail.com>
2021-12-04 13:11:20 +0100

    Add Camera Follow

---
+++ Assets/Scripts/CameraControllerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerObject;
+    [SerializeField] [RangeAttribute(0.01f,1f)]
+    float lerpFollowSpeed = 0.5f;
+    Vector3 posOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOffset = transform.position - playerObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position,playerObject.transform.position + posOffset,lerpFollowSpeed);
+    }
+}
