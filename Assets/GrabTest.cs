b02855e02cdec8b9b149e3f623437bcaccef321a
Marcus <Marcus@PP.local>
2021-12-04 06:42:15 +0100

    Update GrabTest.cs, GrabTest.cs.meta, and 15 more files...

---
+++ Assets/GrabTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabTest : MonoBehaviour
+{
+    private bool isGrabbed = false;
+    private GameObject grabber = null;
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("Hit!");
+        collision.gameObject.GetComponentInParent<HookShotBehaviour>().Hook(this.transform);
+        isGrabbed = true;
+        grabber = collision.gameObject;
+    }
+
+    private void Update() {
+        //if (isGrabbed) {
+        //    transform.position = grabber.transform.position;
+        //}
+    }
+}
